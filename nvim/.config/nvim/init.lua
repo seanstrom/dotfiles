1d0a615443774fd9754594a6ed0f3fbd7aaa4d2b
Sean Hagstrom <sean@seanhagstrom.com>
2022-05-29 14:51:31 +0100

    remove unused lua config for neovim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,9 +1,1 @@
 vim.cmd('source ~/.config/nvim/vim/init.vim')
-
--- local cmd = vim.cmd
--- local function fg(group, color)
---    cmd("hi " .. group .. " guifg=" .. color)
--- end
-
--- vim.cmd [[ autocmd BufEnter,BufWinEnter,FileType,WinEnter * lua require("status-line").hide_statusline() ]]
--- fg("StatusLineNC", "white gui=underline")
