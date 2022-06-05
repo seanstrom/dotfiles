5c069f51b61e8f499d5ffff3279454a953cefe92
Sean Hagstrom <sean@seanhagstrom.com>
2022-06-05 10:11:43 +0100

    add basic autocomplete support for neovim, add lsp support for fsharp, add autocomplete support nvim buffers

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,1 +1,30 @@
 vim.cmd('source ~/.config/nvim/vim/init.vim')
+
+vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+
+local cmp = require 'cmp'
+cmp.setup {
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'buffer' },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
+    ['<C-p>'] = cmp.mapping(cmp.mapping.select_prev_item(), {'i','c'}),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  })
+}
+
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+
+local nvim_lsp = require("lspconfig")
+
+nvim_lsp.fsautocomplete.setup {
+  capabilities = capabilities,
+  cmd = { "dotnet", "fsautocomplete", "--background-service-enabled" },
+}
