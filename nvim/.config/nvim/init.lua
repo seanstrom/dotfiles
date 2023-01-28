84ae5547764aae00d828e087779138cca2fc0dbb
Sean Hagstrom <sean@seanhagstrom.com>
2023-01-28 10:39:41 +0000

    update neovide with lsp, formatting, and better leader keys

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,3 +1,7 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+vim.cmd("let g:polyglot_disabled = ['ftdetect', 'autoindent']")
+
 vim.cmd('source ~/.config/nvim/vim/init.vim')
 
 require('gitsigns').setup()
@@ -22,7 +26,8 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'buffer' },
-    { name = 'path' }
+    { name = 'path' },
+    { name = 'conjure' },
   },
   mapping = cmp.mapping.preset.insert({
     ['<C-n>'] = cmp.mapping(cmp.mapping.select_next_item(), {'i','c'}),
@@ -35,12 +40,35 @@ cmp.setup {
   })
 }
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
-
+local capabilities = require('cmp_nvim_lsp').default_capabilities()
 local nvim_lsp = require("lspconfig")
+local nvim_format = require("lsp-format")
+
+nvim_format.setup({}) 
 
 nvim_lsp.fsautocomplete.setup {
   capabilities = capabilities,
   cmd = { "dotnet", "fsautocomplete", "--background-service-enabled" },
 }
+
+nvim_lsp["clangd"].setup({
+    capabilities = capabilities,
+    cmd = {"clangd", "--compile-commands-dir=./"}
+})
+
+nvim_lsp["clojure_lsp"].setup({
+    capabilities = capabilities,
+    on_attach = nvim_format.on_attach
+})
+
+nvim_lsp.elmls.setup({ capabilities = capabilities, on_attach = nvim_format.on_attach })
+
+-- Treesitter Setup
+
+local treesitter = require('nvim-treesitter.configs')
+treesitter.setup({
+    highlight = { enable = true },
+    indent = { enable = true },
+    ensure_installed = { "javascript", "elm", "clojure", "lua", "fennel", "python", "c_sharp", "rust", "cpp", "html", "css" }
+})
+
